afb99500e7e882b0c5d01a793681c3953b54266c
Anand <anand@sparkouttech.com>
2023-12-12 12:57:21 +0530

    setup

--- Tests/EmojiStarRatingsTests/EmojiStarRatingsTests.swift
+++ Tests/EmojiStarRatingsTests/EmojiStarRatingsTests.swift
@@ -6,6 +6,6 @@ final class EmojiStarRatingsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(EmojiStarRatings().text, "Hello, World!")
+        XCTAssertEqual("ratings", "Hello, World!")
     }
 }
